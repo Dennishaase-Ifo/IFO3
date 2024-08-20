f657113e1d5a923a2d9e57b3bb7fd7356569bcb4
Thore Reinholz <thorerei3@gmail.com>
2024-08-20 17:31:56 +0200

    initial GUI

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 namespace Project1 {
 
@@ -35,11 +35,43 @@ namespace Project1 {
 			}
 		}
 
+	protected:
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^  dateiToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  toolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^  toolStripMenuItem2;
+	private: System::Windows::Forms::ToolStripMenuItem^  neuToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  speichernToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  ladenToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  exportToolStripMenuItem;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
+
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::TextBox^  textBox5;
+	private: System::Windows::Forms::TextBox^  textBox6;
+	private: System::Windows::Forms::TextBox^  textBox7;
+	private: System::Windows::Forms::TextBox^  textBox8;
+	private: System::Windows::Forms::TextBox^  textBox9;
+	private: System::Windows::Forms::TextBox^  textBox10;
+	private: System::Windows::Forms::TextBox^  textBox11;
+	private: System::Windows::Forms::TextBox^  textBox12;
+	private: System::Windows::Forms::TextBox^  textBox13;
+	private: System::Windows::Forms::TextBox^  textBox14;
+	private: System::ComponentModel::IContainer^  components;
+
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,18 +80,278 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->dateiToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem2 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->neuToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->speichernToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ladenToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->exportToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox10 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox11 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox12 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox13 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox14 = (gcnew System::Windows::Forms::TextBox());
+			this->menuStrip1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->dateiToolStripMenuItem,
+					this->toolStripMenuItem1, this->toolStripMenuItem2
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(535, 24);
+			this->menuStrip1->TabIndex = 1;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// dateiToolStripMenuItem
+			// 
+			this->dateiToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->neuToolStripMenuItem,
+					this->speichernToolStripMenuItem, this->ladenToolStripMenuItem, this->exportToolStripMenuItem
+			});
+			this->dateiToolStripMenuItem->Name = L"dateiToolStripMenuItem";
+			this->dateiToolStripMenuItem->Size = System::Drawing::Size(46, 20);
+			this->dateiToolStripMenuItem->Text = L"Datei";
+			// 
+			// toolStripMenuItem1
+			// 
+			this->toolStripMenuItem1->Alignment = System::Windows::Forms::ToolStripItemAlignment::Right;
+			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
+			this->toolStripMenuItem1->Size = System::Drawing::Size(24, 20);
+			this->toolStripMenuItem1->Text = L"\?";
+			// 
+			// toolStripMenuItem2
+			// 
+			this->toolStripMenuItem2->Alignment = System::Windows::Forms::ToolStripItemAlignment::Right;
+			this->toolStripMenuItem2->Name = L"toolStripMenuItem2";
+			this->toolStripMenuItem2->Size = System::Drawing::Size(31, 20);
+			this->toolStripMenuItem2->Text = L"🔍";
+			// 
+			// neuToolStripMenuItem
+			// 
+			this->neuToolStripMenuItem->Name = L"neuToolStripMenuItem";
+			this->neuToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->neuToolStripMenuItem->Text = L"Neu";
+			// 
+			// speichernToolStripMenuItem
+			// 
+			this->speichernToolStripMenuItem->Name = L"speichernToolStripMenuItem";
+			this->speichernToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->speichernToolStripMenuItem->Text = L"Speichern";
+			// 
+			// ladenToolStripMenuItem
+			// 
+			this->ladenToolStripMenuItem->Name = L"ladenToolStripMenuItem";
+			this->ladenToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->ladenToolStripMenuItem->Text = L"Laden";
+			// 
+			// exportToolStripMenuItem
+			// 
+			this->exportToolStripMenuItem->Name = L"exportToolStripMenuItem";
+			this->exportToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->exportToolStripMenuItem->Text = L"Export";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(102, 321);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"<---";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(184, 320);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"Ändern";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(265, 320);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 4;
+			this->button3->Text = L"Löschen";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(347, 319);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 5;
+			this->button4->Text = L"--->";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->pictureBox1->Location = System::Drawing::Point(67, 155);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(391, 10);
+			this->pictureBox1->TabIndex = 6;
+			this->pictureBox1->TabStop = false;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(160, 58);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 9;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(160, 85);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 10;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(160, 112);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 11;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(276, 58);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 12;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(276, 85);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(100, 20);
+			this->textBox5->TabIndex = 13;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(276, 112);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(100, 20);
+			this->textBox6->TabIndex = 14;
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(160, 185);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(100, 20);
+			this->textBox7->TabIndex = 15;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(160, 212);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(100, 20);
+			this->textBox8->TabIndex = 16;
+			// 
+			// textBox9
+			// 
+			this->textBox9->Location = System::Drawing::Point(160, 239);
+			this->textBox9->Name = L"textBox9";
+			this->textBox9->Size = System::Drawing::Size(100, 20);
+			this->textBox9->TabIndex = 17;
+			// 
+			// textBox10
+			// 
+			this->textBox10->Location = System::Drawing::Point(160, 266);
+			this->textBox10->Name = L"textBox10";
+			this->textBox10->Size = System::Drawing::Size(100, 20);
+			this->textBox10->TabIndex = 18;
+			// 
+			// textBox11
+			// 
+			this->textBox11->Location = System::Drawing::Point(267, 185);
+			this->textBox11->Name = L"textBox11";
+			this->textBox11->Size = System::Drawing::Size(100, 20);
+			this->textBox11->TabIndex = 19;
+			// 
+			// textBox12
+			// 
+			this->textBox12->Location = System::Drawing::Point(267, 212);
+			this->textBox12->Name = L"textBox12";
+			this->textBox12->Size = System::Drawing::Size(100, 20);
+			this->textBox12->TabIndex = 20;
+			// 
+			// textBox13
+			// 
+			this->textBox13->Location = System::Drawing::Point(267, 239);
+			this->textBox13->Name = L"textBox13";
+			this->textBox13->Size = System::Drawing::Size(100, 20);
+			this->textBox13->TabIndex = 21;
+			// 
+			// textBox14
+			// 
+			this->textBox14->Location = System::Drawing::Point(267, 266);
+			this->textBox14->Name = L"textBox14";
+			this->textBox14->Size = System::Drawing::Size(100, 20);
+			this->textBox14->TabIndex = 22;
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(910, 554);
+			this->ClientSize = System::Drawing::Size(535, 384);
+			this->Controls->Add(this->textBox14);
+			this->Controls->Add(this->textBox13);
+			this->Controls->Add(this->textBox12);
+			this->Controls->Add(this->textBox11);
+			this->Controls->Add(this->textBox10);
+			this->Controls->Add(this->textBox9);
+			this->Controls->Add(this->textBox8);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->menuStrip1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
 }
